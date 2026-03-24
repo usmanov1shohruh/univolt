111066e5210bc52cce6ad0b7a7d90706e4fb52e9
usmanov1shohruh <usmanov.shohruh01@gmail.com>
2026-03-24 10:24:30 +0500

    Enable CORS for Vercel and Telegram

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['https://univolt-blush.vercel.app', 'https://web.telegram.org'],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
