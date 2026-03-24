8e92866c07064c5cc52d73a7a50f15ca57f507ef
usmanov1shohruh <usmanov.shohruh01@gmail.com>
2026-03-24 10:46:39 +0500

    Fix main.ts bootstrap and CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: ['https://univolt-blush.vercel.app', 'https://web.telegram.org'],
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
+  app.enableCors({ origin: true });
 
   await app.listen(process.env.PORT ?? 3000);
 }
+bootstrap();
